61a9b4508d3f50373c95ef6bb0b3a5cee52ac511
joelleakilimali <akilimalijoelle@gmail.com>
2023-01-06 23:59:34 +0200

    second backend url

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,6 +4,7 @@ export const useFetch = (url) => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
+
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
