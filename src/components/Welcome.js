982739e85991d5c8ad75443c5164ef68dfa59cb3
joelleakilimali <akilimalijoelle@gmail.com>
2023-01-07 11:50:36 +0200

    loader modified

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,7 +3,7 @@ import { BiUser } from "react-icons/bi";
 import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
-
+import Loader from "./Loader";
 const Welcome = () => {
   let navigate = useNavigate();
   const pushtoMail = () => {
@@ -12,31 +12,37 @@ const Welcome = () => {
   const { data, loading, error } = useFetch("/emails/countInbox");
 
   return (
-    <div className="flex justify-center h-screen w-screen flex-col items-center bg-black">
-      <div className="flex flex-col bg-white h-64 rounded-xl w-[800px] p-3 ">
-        <div className="flex justify-between pb-5">
-          <span className="border-2 border-black rounded-full p-2 ">
-            <BiUser size={30} />
-          </span>
-          <h1>Joelle Akilimali</h1>
-        </div>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="flex justify-center h-screen w-screen flex-col items-center bg-black">
+          <div className="flex flex-col bg-white h-64 rounded-xl w-[800px] p-3 ">
+            <div className="flex justify-between pb-5">
+              <span className="border-2 border-black rounded-full p-2 ">
+                <BiUser size={30} />
+              </span>
+              <h1>Joelle Akilimali</h1>
+            </div>
 
-        <hr />
-        <div className="pt-18">
-          <p className="">Welcome back into your Account Dear</p>
-          <ul>
-            <li> Total Message: {data[0]?.count}</li>
-            <li> Unread Message: {data[1]?.unread}</li>
-          </ul>
-          <button
-            className="bg-red-400 w-[200px] mt-6 py-2 rounded-lg"
-            onClick={pushtoMail}
-          >
-            View Messages
-          </button>
+            <hr />
+            <div className="pt-18">
+              <p className="">Welcome back into your Account Dear</p>
+              <ul>
+                <li> Total Message: {data[0]?.count}</li>
+                <li> Unread Message: {data[1]?.unread}</li>
+              </ul>
+              <button
+                className="bg-red-400 w-[200px] mt-6 py-2 rounded-lg"
+                onClick={pushtoMail}
+              >
+                View Messages
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
