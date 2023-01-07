3e1b499a0739f3a768e89ff2f7a1e56979acc13c
joelleakilimali <akilimalijoelle@gmail.com>
2023-01-07 13:50:07 +0200

    creation of instance instead of using axios

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -4,10 +4,10 @@ import { FaSpinner } from "react-icons/fa";
 const Loader = () => {
   return (
     <div className="loader flex flex-row mt-10 pt-60 justify-center  align-middle border-gray-50 border-l-rose-300">
+      <p className="mt-5 px-10 text-6xl">Please wait . . . </p>
       <div className="px-55">
         <FaSpinner size={80} className="spinner " />
       </div>
-      <p className="mt-5 px-10 text-6xl">Please wait . . . </p>
     </div>
   );
 };
